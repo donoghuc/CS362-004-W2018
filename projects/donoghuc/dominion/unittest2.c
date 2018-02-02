40d5664f9636305a08927cdef73d3ef75e113d4c
donoghuc <cas.donoghue@gmail.com>
2018-02-02 13:00:27 -0800

    refactor buyCard to allow new card to enter via the dicard pile

--- projects/donoghuc/dominion/unittest2.c
+++ projects/donoghuc/dominion/unittest2.c
@@ -3,19 +3,19 @@
 
 int compare(const int* a, const int* b);
 // print the cards in a player's hand
-void print_hand(int player, struct gameState *state){
-    int i;
-    for (i=0; i < state->handCount[player]; i++){
-        printf("%d ", state->hand[player][i]);
-    }
-    printf("\n");
-}
+// void print_hand(int player, struct gameState *state){
+//     int i;
+//     for (i=0; i < state->handCount[player]; i++){
+//         printf("%d ", state->hand[player][i]);
+//     }
+//     printf("\n");
+// }
 // this function is built to count a card (supplyPos) in a players hand
 int get_card_count(int supplyPos, int player, struct gameState *state){
     int i;
     int counter=0;
-    for (i=0; i < state->handCount[player]; i++){
-        if(state->hand[player][i] == supplyPos){
+    for (i=0; i < state->discardCount[player]; i++){
+        if(state->discard[player][i] == supplyPos){
         	counter++;
         }
     }
@@ -32,38 +32,33 @@ int main () {
   // set up test for functionality, buy a village card, expect it to be in hand
   int village_pre;
   int village_post; 
+  int discard_count_pre = G.discardCount[0];
   initializeGame(2, k, 10, &G);
   printf("Player's Turn: %d\n", G.whoseTurn);
   printf("Num coins to spend pre buyCard: %d\n", G.coins);
-  printf("Hand Count pre buyCard: %d\n", G.handCount[0]);
+  printf("Player 0 discard pre buy\n");
+  printDiscard(0,&G); 
   printf("Village supply pre buyCard: %d\n", G.supplyCount[village]);
   printf("Num buys pre buyCard: %d\n", G.numBuys);
-  printf("execute buy card\n");
-  printf("hand pre buy village: \n");
-  print_hand(0,&G);
   village_pre = get_card_count(village,0,&G);
-  printf("number village cards in hand pre buy: %d\n",village_pre);
-
+  printf("number village cards in discard pre buy: %d\n",village_pre);
+  // execute function
+  printf("execute buy card\n");
   int ret = buyCard(village, &G);
   if (ret != 0){
     printf("ERROR: BUY CARD NON ZERO RETURN VALUE\n");
     fail++;
   }
   printf("Num coins to spend post buyCard: %d\n", G.coins);
-  printf("Hand Count post buyCard: %d\n", G.handCount[0]);
   printf("Village supply post buyCard: %d\n", G.supplyCount[village]);
   printf("Num buys post buyCard: %d\n", G.numBuys);
-  printf("hand post buy village: \n");
-  print_hand(0,&G);
+  printf("Player 0 discard post buy");
+  printDiscard(0,&G); 
   village_post = get_card_count(village,0,&G);
-  printf("number village cards in hand post buy: %d\n",village_post);
+  printf("number village cards in discard post buy: %d\n",village_post);
   // evaluate based on setup
   if(G.coins != 1){
   	printf("COIN ARITHMATIC FAIL\n");
-  	fail++;
-  }
-  if(G.handCount[0] != 6){
-  	printf("FAILURE TO INCREMENT HAND COUNT\n");
   	fail++;
   }
   if(G.supplyCount[village] != 9){
@@ -78,13 +73,17 @@ int main () {
   	printf("FAILURE TO ADD CARD TO HAND\n");
   	fail++;
   }
+  if(G.discardCount[0] - discard_count_pre != 1){
+    printf("FAILURE TO INCREMEMNT DISCARD PILE WITH NEW CARD\n");
+    fail++;  
+  }
   // reinitialize and test failure mode 
   printf("Check case where no buys\n");
   initializeGame(2, k, 10, &G);
   G.numBuys = 0;
   ret = buyCard(village, &G);
   if(ret != -1){
-  	printf("FAILURE TO VALIDATE NUMB BUYS\n");
+  	printf("FAILURE TO VALIDATE NUM BUYS\n");
   	fail++; 	
   } 
   // reinitialize and test failure mode 
