c80f0ff76c708d0a138d20b5817082241ba6bc4f
donoghuc <cas.donoghue@gmail.com>
2018-02-10 12:03:38 -0800

    continue with adventurer random test dev

--- projects/donoghuc/dominion/randomtestadventurer.c
+++ projects/donoghuc/dominion/randomtestadventurer.c
@@ -4,16 +4,18 @@
 #include "math.h"
 #include <stdio.h>
 
+#define DEBUG 0
+
+// data structure for keepin track of treasure totals
 struct totals{
     int total;
     int hand_treasure;
     int deck_treasure;
     int disc_treasure; 
-}
+};
 
 // return a psudo random integer in given range (inclusive)
 int rand_between(int min, int max){
-
     return rand() % (max + 1 - min) + min;
 }
 
@@ -27,22 +29,62 @@ int is_treasure(int card){
     }
 }
 
+// for debuging print out the treasure values
+void print_totals(struct totals *T){
+    printf("total: %d\n hand_treasure: %d\n deck_treasure: %d\n disc_treasure: %d\n",
+        T->total, T->hand_treasure, T->deck_treasure, T->disc_treasure);
+}
+
+// this is the number of treasure cards possible for adventurer to find (deck treasure plus discard treasure)
+int possible_treasure(struct totals *T){
+    return T->deck_treasure + T->disc_treasure;
+}
+
+// count the treasure for a given player for use in comparing adventurer effect
+void count_treasure(int player, struct gameState *G, struct totals *T){
+    T->hand_treasure = 0; 
+    T->deck_treasure = 0;
+    T->disc_treasure = 0;
+    int i;
+    for (i=0;i<G->handCount[player];i++){
+        if(is_treasure(G->hand[player][i]) == 0){
+            T->hand_treasure++;
+        }
+    }
+    for (i=0;i<G->deckCount[player];i++){
+        if(is_treasure(G->deck[player][i]) == 0){
+            T->deck_treasure++;
+        }
+    }
+    for (i=0;i<G->discardCount[player];i++){
+        if(is_treasure(G->discard[player][i]) == 0){
+            T->disc_treasure++;
+        }
+    }
+
+    // printf("%d\n", G->handCount[0]);
+    T->total = G->handCount[player] + G->deckCount[player] + G->discardCount[player];
+}
+
+
 /* generate a random game state with variables relevant to adventurer effect*/
-int generate_random_gamestate(int player, struct gameState *G, struct totals *T){
+void generate_random_gamestate(int player, struct gameState *G){
     // have a total number of cards to distribute, the hand must have at least one care (adventurer)
-    int total = rand_between(1,15);
+    int total = rand_between(1,500);
     int num_deck = rand_between(0, total - 1);
     int num_hand = rand_between(1, total - num_deck);
     int num_disc = total - num_deck - num_hand;
-    printf("num_deck: %d\nnum_hand: %d\nnum_disc: %d\n sum: %d\n total: %d\n", num_deck, num_hand, num_disc,(num_deck+num_hand+num_disc), total);
+#if (DEBUG == 1)
+    printf("num_deck: %d\nnum_hand: %d\nnum_disc: %d\n sum: %d\n total: %d\n",
+                 num_deck, num_hand, num_disc,(num_deck+num_hand+num_disc), total);
+#endif  
     // distribute cards into deck, hand and discard (random cards between 0 and 26 inclusive)
+
     int i;
     G->deckCount[player] = num_deck;
     for (i=0; i < num_deck; i++);
         G->deck[player][i] = rand_between(0, 26);
-        if(is_treasure(G->deck[player][i]) == 0){
-            T->deck_treasure++; 
-        }
+
 
     G->handCount[player] = num_hand;
     for (i=0; i < num_hand; i++);
@@ -54,7 +96,6 @@ int generate_random_gamestate(int player, struct gameState *G, struct totals *T)
 
     G->hand[G->whoseTurn][player] = adventurer;
 
-    T->total = total;
 }
 
 
@@ -65,15 +106,30 @@ int main () {
     int fail = 0; 
     // initialize a game state (pre randomization)
     struct gameState G;
+    struct totals T_pre;
+    struct totals T_post;
+    // this will be overwritten...
     int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse,
            sea_hag, tribute, smithy};
     initializeGame(2, k, 10, &G);
+
     int total_cards; //variable for comparing total cards allocated for test
+    int possible; // With random generation of cards there may not be two treasure to possibly add
+    //variables to build random call signature. 
+    int choice1;
+    int choice2;
+    int choice3;
+    int handPos;
+    int bonus;
 
     // execute random testing
     int i = 10; 
     while(i >= 0){
-        total_cards = generate_random_gamestate(0, &G);
+        generate_random_gamestate(0, &G);
+        count_treasure(0, &G, &T_pre);
+        
+#if (DEBUG == 1)
+        print_totals(&T_pre);
         printHand(0,&G);
         printf("Hand Count: %d\n", G.handCount[0]);
         printDiscard(0,&G);
@@ -82,7 +138,17 @@ int main () {
         printf("Deck Count: %d\n", G.deckCount[0]);
         printPlayed(0,&G);
         printf("Played Count: %d\n", G.playedCardCount);
-        int ret = cardEffect(adventurer, -1,-1,-1,&G,0,0);
+#endif        
+        int choice1 = rand_between(-500,500);
+        int choice2 = rand_between(-500,500);
+        int choice3 = rand_between(-500,500);
+        int handPos = rand_between(-500,500);
+        int bonus = rand_between(-500,500);
+        int ret = cardEffect(adventurer, choice1,choice2,choice3,&G,handPos,bonus);
+        count_treasure(0, &G, &T_post);
+        
+#if (DEBUG == 1)
+        print_totals(&T_post);
         printHand(0,&G);
         printf("Hand Count: %d\n", G.handCount[0]);
         printDiscard(0,&G);
@@ -90,7 +156,47 @@ int main () {
         printDeck(0,&G);
         printf("Deck Count: %d\n", G.deckCount[0]);
         printPlayed(0,&G);
-        printf("Played Count: %d\n", G.playedCardCount);      
+        printf("Played Count: %d\n", G.playedCardCount);   
+#endif    
+        if (ret != 0){
+            fail++;
+            printf("FAIL: non zero return value for card effect");
+        }
+        possible = possible_treasure(&T_pre);
+        if (possible >= 2){
+            if (T_pre.total != T_post.total){
+                fail++;
+                printf("FAIL: Total pre: %d\n total post: %d\n", T_pre.total, T_post.total);
+            }
+            if (T_post.hand_treasure - T_pre.hand_treasure != 2){
+                fail++;
+                printf("FAIL: Two treasure not gained\n");
+            }
+            if (G.hand[0][0] == adventurer){
+                fail++;
+                printf("FAIL: adventurer not discarded\n");
+            }
+            if (G.handCount[0] - (G.discardCount[0] + G.deckCount[0]) != 2){
+                fail++;
+                printf("FAIL: handcount %d discardCount: %d deckCount: %d expected difference of 2\n", G.handCount[0], G.discardCount[0],G.deckCount[0]);
+            }
+            
+
+        } else {
+            if (T_pre.total != T_post.total){
+                fail++;
+                printf("FAIL: Total pre: %d\n total post: %d\n", T_pre.total, T_post.total);
+            }
+            if ((T_post.hand_treasure - T_pre.hand_treasure) != possible){
+                fail++;
+                printf("FAIL: %d treasure not gained\n", possible);
+            }
+            if (G.hand[0][0] == adventurer){
+                fail++;
+                printf("FAIL: adventurer not discarded\n");
+            }
+        }
+
         i--;  
     }
 
